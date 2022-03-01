06a0791dbae05e4e17667344c3e5dec0de4f9d65
Terrence <xiaotiannj@gmail.com>
2022-03-01 23:15:24 +0800

    Init

---
+++ src/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,31 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
+    public TankFrame(){
+        setSize(800, 600);
+        setTitle("Tank War");
+        setResizable(false);
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.fillRect(x,y,50,50);
+
+        x += 10;
+        y += 10;
+    }
+}
