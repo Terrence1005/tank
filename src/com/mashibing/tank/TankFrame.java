8525be6c74391046bfc325be7b14f2320f67a7bb
Terrence <xiaotiannj@gmail.com>
2022-03-03 23:23:18 +0800

    Modify

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -29,20 +29,75 @@ public class TankFrame extends Frame {
     public void paint(Graphics g){
         g.fillRect(x,y,50,50);
 
-        x += 10;
+//        x += 10;
 //        y += 10;
     }
 
     class MyKeyListener extends KeyAdapter{
-
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println("key pressed");
+
+            int key = e.getKeyCode();
+            switch(key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+            move();
+
+
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch(key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
+            move();
+        }
+
+        public void move(){
+            if(bL){
+                x -= 10;
+            }
+            if(bR){
+                x += 10;
+            }
+            if(bU){
+                y -= 10;
+            }
+            if(bD){
+                y += 10;
+            }
         }
     }
 }
